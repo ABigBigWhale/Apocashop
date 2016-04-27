84dfd277298a758b1b07d272e6ac229f6ea9a745
Hao Liu <liuh25@D-172-28-35-165.dhcp4.washington.edu>
2016-04-26 22:17:56 -0700

    added buttons and item slots

--- game/js/apocashop.js
+++ game/js/apocashop.js
@@ -11,28 +11,65 @@ var gameConfig = {
         'gameDiv',                              // DOM object to insert canvas
         { preload: preload, create: create }    // Function references
     );
-    
+
     if (gameConfig.DEBUG_MODE) window.game = game;
 
     function preload() {
         game.load.image(assets.image.background.id, assets.image.background.url);
-	game.load.image(assets.image.ui.items.id, assets.image.ui.items.url);
-	game.load.image(assets.image.ui.dialog.id, assets.image.ui.dialog.url);
+        game.load.image(assets.image.ui.itemslot.id, assets.image.ui.itemslot.url);
+        game.load.image(assets.image.ui.dialog.id, assets.image.ui.dialog.url);
+        
+        game.load.image(assets.image.ui.button.accept.idle.id,
+                       assets.image.ui.button.accept.idle.url);
+        game.load.image(assets.image.ui.button.accept.idle.id,
+                       assets.image.ui.button.accept.idle.url);
+        game.load.image(assets.image.ui.button.accept.idle.id,
+                       assets.image.ui.button.accept.idle.url);
+        
+        game.load.image(assets.image.ui.button.reject.idle.id,
+                       assets.image.ui.button.reject.idle.url);
+        game.load.image(assets.image.ui.button.reject.idle.id,
+                       assets.image.ui.button.reject.idle.url);
+        game.load.image(assets.image.ui.button.reject.idle.id,
+                       assets.image.ui.button.reject.idle.url);
+        
+        game.load.image(assets.image.ui.button.question.idle.id,
+                       assets.image.ui.button.question.idle.url);
+        game.load.image(assets.image.ui.button.question.idle.id,
+                       assets.image.ui.button.question.idle.url);
+        game.load.image(assets.image.ui.button.question.idle.id,
+                       assets.image.ui.button.question.idle.url);
     }
-    
+
+    // TODO: Lots of hard-coding right now
     function create() {
         game.renderer.renderSession.roundPixels = true;
         Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
         game.stage.smoothed = false;
-        
+
         game.stage.backgroundColor = '#447474';
-        
+
         var imgBackground = game.add.image(0, 0, assets.image.background.id);
-	//imgBackground.set.anchor(1, 0);
-	var uiItems = game.add.sprite(0, 400, assets.image.ui.items.id);
-	uiItems.anchor.setTo(0, 1);
-	var uiDialog = game.add.sprite(0, 400, assets.image.ui.dialog.id);
+        
+        for (var i = 0; i < 4; i++) {
+            var uiItemslot = game.add.sprite(10, 10 + 60 * i, assets.image.ui.itemslot.id);
+            uiItemslot.anchor.setTo(0, 0);
+        }
+        
+        var uiDialog = game.add.sprite(0, 400, assets.image.ui.dialog.id);
+        
+        var uiButtonAccept = game.add.sprite(660, 420, assets.image.ui.button.accept.idle.id);
+        var uiButtonReject = game.add.sprite(660, 480, assets.image.ui.button.reject.idle.id);
+        var uiButtonQuestion = game.add.sprite(660, 540, assets.image.ui.button.question.idle.id);
+        
+        uiButtonAccept.scale.setTo(2, 2);
+        uiButtonReject.scale.setTo(2, 2);
+        uiButtonQuestion.scale.setTo(2, 2);
+        
+        uiButtonAccept.smoothed = false;
+        uiButtonReject.smoothed = false;
+        uiButtonQuestion.smoothed = false;
     }
-    
+
 })();
 
--- game/js/assets.js
+++ game/js/assets.js
@@ -1,29 +1,76 @@
 var assets = {
-    
+
     image : {
-        
+
         background : {
             id : 'background',
             url: 'assets/background.png'
         },
-        
+
         ui : {
-            
-            items: {
-                id : 'ui_items1',
-                url: 'assets/ui_items1.png'
+
+            itemslot: {
+                id : 'ui_itemslot',
+                url: 'assets/ui_itemslot.png'
             },
-            
+
             dialog: {
                 id : 'ui_dialog',
                 url: 'assets/ui_dialog.png'
+            },
+
+            button: {
+
+                accept: {
+                    idle: {
+                        id : 'ui_button_accept',
+                        url: 'assets/ui_button_accept.png'
+                    },
+                    hover: {
+                        id : 'ui_button_accept_hover',
+                        url: 'assets/ui_button_accept_hover.png'
+                    },
+                    pressed: {
+                        id : 'ui_button_accept_pressed',
+                        url: 'assets/ui_button_accept_pressed.png'
+                    }
+                },
+
+                reject: {
+                    idle: {
+                        id : 'ui_button_reject',
+                        url: 'assets/ui_button_reject.png'
+                    },
+                    hover: {
+                        id : 'ui_button_reject_hover',
+                        url: 'assets/ui_button_reject_hover.png'
+                    },
+                    pressed: {
+                        id : 'ui_button_reject_pressed',
+                        url: 'assets/ui_button_reject_pressed.png'
+                    }
+                },
+
+                question: {
+                    idle: {
+                        id : 'ui_button_question',
+                        url: 'assets/ui_button_question.png'
+                    },
+                    hover: {
+                        id : 'ui_button_question_hover',
+                        url: 'assets/ui_button_question_hover.png'
+                    },
+                    pressed: {
+                        id : 'ui_button_question_pressed',
+                        url: 'assets/ui_button_question_pressed.png'
+                    }
+                }
             }
-            
-        }
-        
+        },
+
     },
-    
+
     sound : {
-        
+
     }
 }
