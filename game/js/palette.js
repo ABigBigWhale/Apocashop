d4fa9497e9fe3912d22ef1d25e05fe7cf4e8e186
Hao Liu <liuh25@crope.cs.washington.edu>
2016-05-03 14:40:02 -0700

    changed item drop anim

---
+++ game/js/palette.js
@@ -0,0 +1,19 @@
+var Colors = {
+  
+  // Used for non-interactive items such as 
+  // dialog boxes
+  PassiveLighter: '#ffffca',
+  PassiveLight:   '#af9165',
+  PassiveMain:    '#4d372c',
+  PassiveDark:    '#4d372c',
+  PassiveDarker:  '#4d361e',
+  PassiveDarkest: '#2e2821',  // Recommended only for shadows
+  
+  // Interactive items such as buttons
+  ActiveLight:  '#cfcece',
+  ActiveMain:   '#acacac',
+  ActiveDark:   '#575757',
+  
+  AccentAccept: '#92cb9a',
+  AccentReject: '#cb929a',
+};
