2c4266adddb2ec7d557d5e6b5a0c8df0ecd76bb5
Kyle Owsen <owsenk@cs.washington.edu>
2016-05-04 23:40:19 -0700

    Removed redundant NPCs

--- game/js/backend/resources.js
+++ game/js/backend/resources.js
@@ -171,7 +171,7 @@ var heroes = {
 			success : "Thank you my friend!",
 			fail : "Fine, I'll go elsewhere.",
 			questions : {
-				color : "Though I love many colors, my favorite is orange./It's the greatest of colors, because it's so ... @@@@florange?",
+				color : "Though I love many colors, my favorite is orange./It's the greatest of colors, because it's so . . . @@@@florange?",
 				day : "Good.",
 				default : "What?"
 			},
@@ -237,57 +237,6 @@ var heroes = {
 			appearConditions : ['hidMan'],
 			successConditions : ['soldMan']
 		}
-	},
-	"man" : {
-		type : "interact",
-		item : "none",
-		appearanceInfo : "MAN",
-		offers : [0],
-		offerText : "Yo, please hide me",
-		success : "Thanks!",
-		fail : "Well, guess I'll die then.",
-		questions : {
-			default : "Please, no time."
-		},
-		items : {
-			default : "Please, no time.."
-		},
-		profiles : {
-			default : "Please, no time."
-		},
-		sellConditions : ["hidMan"],
-		refuseConditions : ["manDied"]
-	},
-	"tracker" : {
-		type : "interact",
-		item : "none",
-		appearanceInfo : "TRACKER",
-		offers : [10, 15],
-		offerText : [
-			"A very dangerous man is loose and I need to find him./I'll pay you ten gold for any information."
-		, {
-			hidPoorly : "I know he is here. WHERE IS HE!",
-			default : "How about 15? I know you're not making enough to feed your family."
-		}],
-		success : "You won't get away this time!",
-		fail : "Very well,@@ I hope you've been honest with me./@@@@For your sake.",
-		questions : {
-			day : "Busy.",
-			news : "I just told you the news.",
-			default : "Stop wasting my time."
-		},
-		items : {
-			shield : "I have no need for that.",
-			default : "Stop wasting my time."
-		},
-		profiles : {
-			dragon : "That is not my concern, leave that to Fosado.",
-			scaredMan : "Aha, That's him!@@ W@h@e@r@e@ @i@s@ @h@e@...",
-			default : "Beneath my concern."
-		},
-		appearConditions : ["hidMan"],
-		sellConditions : ["manDead"],
-		refuseConditions : ["manLived"]
 	}
 };
 
