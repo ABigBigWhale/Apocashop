680a5f0c8e34e500eb635fdf9b6b60c2451a9f19
Kyle Owsen <owsenk@cs.washington.edu>
2016-05-27 22:04:14 -0700

    Cleaned up some unused code in the npc generator

--- game/js/backend/npcGen.js
+++ game/js/backend/npcGen.js
@@ -30,8 +30,8 @@ function initNPCGen(game) {
 	var generateAppearance
 	var generateGreeting;
 	var generateHaggle;
-	var generateItemResponse;
-	var generateProfile;
+	// var generateItemResponse;
+	// var generateProfile;
 	var generateQuestions;
 	var generateThanks;
 	var generateLeave;
@@ -189,127 +189,127 @@ function initNPCGen(game) {
 
 	})();
 
-	(function() {
+	// (function() {
 
-		var items = {
-			sword : [
-				"Looks very sharp.",
-				"Ooh, @pointy.",
-				"Looks very sword-y."
-			],
-			bow : [
-				"Ooh, @pointy.",
-				"Looks scary.",
-				"Wouldn't want to be on the receiving end of that.",
-				"Never been a very good shot with one of those."
-			],
-			shield : [
-				"Looks expensive.",
-				"I wonder if they really are fireproof.",
-				"Wish I had one of those yesterday.",
-				"Ooh, @shiny."
-			],
-			water : [
-				"Looks very wet.",
-				"Ooh,@ water.",
-				"Very important.",
-				"I've run out of things I can say about water."
-			],
-			chicken : [
-				"Smells good.",
-				"Yum yum.",
-				"I need food badly."
-			],
-			default : [
-				"Huh?",
-				"Why are you showing this to me.",
-				"Don't really have an opinion.",
-				"What?",
-				"Why?",
-				"Not sure what you're trying to accomplish here.",
-				"Yes, I get it. @You have items."
-			]
-		}
+	// 	var items = {
+	// 		sword : [
+	// 			"Looks very sharp.",
+	// 			"Ooh, @pointy.",
+	// 			"Looks very sword-y."
+	// 		],
+	// 		bow : [
+	// 			"Ooh, @pointy.",
+	// 			"Looks scary.",
+	// 			"Wouldn't want to be on the receiving end of that.",
+	// 			"Never been a very good shot with one of those."
+	// 		],
+	// 		shield : [
+	// 			"Looks expensive.",
+	// 			"I wonder if they really are fireproof.",
+	// 			"Wish I had one of those yesterday.",
+	// 			"Ooh, @shiny."
+	// 		],
+	// 		water : [
+	// 			"Looks very wet.",
+	// 			"Ooh,@ water.",
+	// 			"Very important.",
+	// 			"I've run out of things I can say about water."
+	// 		],
+	// 		chicken : [
+	// 			"Smells good.",
+	// 			"Yum yum.",
+	// 			"I need food badly."
+	// 		],
+	// 		default : [
+	// 			"Huh?",
+	// 			"Why are you showing this to me.",
+	// 			"Don't really have an opinion.",
+	// 			"What?",
+	// 			"Why?",
+	// 			"Not sure what you're trying to accomplish here.",
+	// 			"Yes, I get it. @You have items."
+	// 		]
+	// 	}
 
-		generateItemResponse = function(item) {
-			var itemSayings = items[item];
-			if(!itemSayings) {
-				itemSayings = items.default;
-			}
+	// 	generateItemResponse = function(item) {
+	// 		var itemSayings = items[item];
+	// 		if(!itemSayings) {
+	// 			itemSayings = items.default;
+	// 		}
 
-			var searchSpace = (Math.random() < 0.75) ? items.default : itemSayings;
+	// 		var searchSpace = (Math.random() < 0.75) ? items.default : itemSayings;
 
-			var index = Math.floor(Math.random() * searchSpace.length);
+	// 		var index = Math.floor(Math.random() * searchSpace.length);
 
-			return searchSpace[index];
-		};
+	// 		return searchSpace[index];
+	// 	};
 
-	})();
+	// })();
 
-	(function() {
+	// (function() {
 
-		var profiles = {
-			dayOne : [
-				"Oh, is that the rhyming guy? /@@Hard to believe he's our hero.",
-				"Oh yeah, I love that guy! He's always rhyming and singing.",
-				"I hate that guy, with his @@rhymes.",
-				"How does he always come up with so many rhymes?"
-			],
-			dayTwo : [
-				"Heh, not the best vocabulary on that one.",
-				"How does someone only learn four words?",
-				"There's no way they only know four words."
-			],
-			dayThree : [
-				"What an odd one.",
-				"He refused to say anything to me. Must be saving up his words for you.",
-				"He always manages to find a way to say what he needs to."
-			],
-			convict : [
-				"Looks like a sketchy guy.",
-				"They're the reason we all want bows. They're scared just looking at them.",
-				"Bleh, do not want."
-			],
-			scaredMan : [
-				"Aww, he looks sad.",
-				"He just tried to hide at my house, seemed really scared.",
-				"I'm not sure about him."
-			],
-			tracker : [
-				"He's been hunting for someone. Not sure who.",
-				"Looks scary."
-			],
-			dragon : [
-				"Don't remind me.",
-				"Terrifying.",
-				"D@o@ @n@o@t@ @w@a@n@t@.",
-				"*Crying sounds*"
-			],
-			default : [
-				"I don't know them",
-				"I'm not really sure what you want from me.",
-				"Huh?",
-				"What?",
-				"Eh?",
-				"No opinion on that.",
-				"Sorry, not sure who that is."
-			]	
-		}
+	// 	var profiles = {
+	// 		dayOne : [
+	// 			"Oh, is that the rhyming guy? /@@Hard to believe he's our hero.",
+	// 			"Oh yeah, I love that guy! He's always rhyming and singing.",
+	// 			"I hate that guy, with his @@rhymes.",
+	// 			"How does he always come up with so many rhymes?"
+	// 		],
+	// 		dayTwo : [
+	// 			"Heh, not the best vocabulary on that one.",
+	// 			"How does someone only learn four words?",
+	// 			"There's no way they only know four words."
+	// 		],
+	// 		dayThree : [
+	// 			"What an odd one.",
+	// 			"He refused to say anything to me. Must be saving up his words for you.",
+	// 			"He always manages to find a way to say what he needs to."
+	// 		],
+	// 		convict : [
+	// 			"Looks like a sketchy guy.",
+	// 			"They're the reason we all want bows. They're scared just looking at them.",
+	// 			"Bleh, do not want."
+	// 		],
+	// 		scaredMan : [
+	// 			"Aww, he looks sad.",
+	// 			"He just tried to hide at my house, seemed really scared.",
+	// 			"I'm not sure about him."
+	// 		],
+	// 		tracker : [
+	// 			"He's been hunting for someone. Not sure who.",
+	// 			"Looks scary."
+	// 		],
+	// 		dragon : [
+	// 			"Don't remind me.",
+	// 			"Terrifying.",
+	// 			"D@o@ @n@o@t@ @w@a@n@t@.",
+	// 			"*Crying sounds*"
+	// 		],
+	// 		default : [
+	// 			"I don't know them",
+	// 			"I'm not really sure what you want from me.",
+	// 			"Huh?",
+	// 			"What?",
+	// 			"Eh?",
+	// 			"No opinion on that.",
+	// 			"Sorry, not sure who that is."
+	// 		]	
+	// 	}
 
-		generateProfile = function(profile) {
-			var responses = profiles[profile];
-			if(!responses) {
-				responses = profiles.default;
-			}
+	// 	generateProfile = function(profile) {
+	// 		var responses = profiles[profile];
+	// 		if(!responses) {
+	// 			responses = profiles.default;
+	// 		}
 
-			var searchSpace = (Math.random() < 0.75) ? profiles.default : responses;
+	// 		var searchSpace = (Math.random() < 0.75) ? profiles.default : responses;
 
-			var index = Math.floor(Math.random() * searchSpace.length);
+	// 		var index = Math.floor(Math.random() * searchSpace.length);
 
-			return searchSpace[index];
-		};
+	// 		return searchSpace[index];
+	// 	};
 
-	})();
+	// })();
 
 	(function() {
 
@@ -400,9 +400,7 @@ function initNPCGen(game) {
 			"Thank you!",
 			"Thanks, see ya!",
 			"Thanks.",
-			"TTYL!",
-			"Thanks scruberoo!",
-			"You're the best. I love you ... Too much?"	
+			"You're the best, pal."	
 		]
 
 		generateThanks = function() {
@@ -420,10 +418,8 @@ function initNPCGen(game) {
 			"You just lost my business.",
 			"There's no way I can afford more.",
 			"Terrible experience. Zero stars.",
-			"I'm a poor soul. Why would you do this to me?",
-			"Looks like I'll be getting the lecture from my mother tonight ...",
-			"If I were a rich man, I would go to this store. But alas, I am not.",
-			"I'll miss the adventures that you wouldn't let me have."
+			"This is a disappointment.",
+			"Do I look rich to you?"
 		]
 
 		generateLeave = function() {
