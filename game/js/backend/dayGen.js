8278ab43dfaafeb6d60941488791aee2749d3ec0
Kyle Owsen <owsenk@cs.washington.edu>
2016-05-29 02:18:34 -0700

    Reduced tax difficulty.

--- game/js/backend/dayGen.js
+++ game/js/backend/dayGen.js
@@ -129,8 +129,8 @@ function initDayGenerator(game) {
 		}
 
 		generateTaxWrapup = function(day, index) {
-			var taxes = (Math.ceil((1 + index) / 4) * 2 - 1) * 10;
-			var nextTax = (Math.ceil((2 + index) / 4) * 2 - 1) * 10
+			var taxes = Math.ceil((1 + index) / 4) * 10;
+			var nextTax = Math.ceil((2 + index) / 4) * 10;
 			day.wrapup.push(generateMessage("You are forced by King Zoran to pay " + taxes + " gold in taxes.", -taxes));
 			if(taxes !== nextTax) {
 				day.wrapup.push(generateMessage("You are warned that tomorrow, Zoran will want " + nextTax + " gold."));
