1211b7c19f16f748e1d7ec54cbe5b0c1e894e478
Carson Lipscomb <carsonal@cs.washington.edu>
2016-05-30 19:47:51 -0700

    added some small delay on startpage so that no error occur in playing music

--- game/js/StartState.js
+++ game/js/StartState.js
@@ -6,10 +6,13 @@ function StartStateWrapper(game) {
 			game.displayManager.prepareStage();
 			game.displayManager.putTitleScreen();
 			game.soundManager.playMusic(game.Music.TITLEMUS);
+			var timeout = new Date().getTime() + 300; // wait 2 seconds
 			game.input.onDown.add(function() {
-				printDebug('STARTING state_play');
-				game.soundManager.stopMusic();
-				game.state.start('state_play');
+				if (new Date().getTime() > timeout) {
+					printDebug('STARTING state_play');
+					game.soundManager.stopMusic();
+					game.state.start('state_play');
+				}
 			});
 		}
 	};
